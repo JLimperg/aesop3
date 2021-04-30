d8aa4e95d22ab23967f639b91025d96108ce7611
Jannis Limperg <jannis@limperg.de>
2021-04-30 15:16:31 +0200

    Move test to separate file

--- src/tactic/aesop/default.lean
+++ src/tactic/aesop/default.lean
@@ -23,32 +23,3 @@ tactic.aesop.aesop
 
 end interactive
 end tactic
-
-/-!
-## Tests
--/
-
-inductive Even : ℕ → Prop
-| zero : Even 0
-| plus_two {n} : Even n → Even (n + 2)
-
-inductive Odd : ℕ → Prop
-| one : Odd 1
-| plus_two {n} : Odd n → Odd (n + 2)
-
-inductive EvenOrOdd : ℕ → Prop
-| even {n} : Even n → EvenOrOdd n
-| odd {n} : Odd n → EvenOrOdd n
-
-attribute [aesop  50] EvenOrOdd.even
-attribute [aesop 100] EvenOrOdd.odd Even.zero Even.plus_two Odd.one
-
-def even_or_odd (n : ℕ) : Prop := EvenOrOdd n
-
-lemma even_or_odd_def {n} : even_or_odd n = EvenOrOdd n := rfl
-
-meta def test_norm_tactic : tactic unit := `[try { rw [even_or_odd_def] at * }]
-
-meta def test_regular_tactic : tactic unit := `[apply Odd.plus_two]
-
-example : even_or_odd 3 := by aesop rules: [test_regular_tactic 50], norm: [test_norm_tactic]
