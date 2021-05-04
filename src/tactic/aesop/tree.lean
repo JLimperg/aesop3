JLimperg/aesop3

commit 1f8e7c8bfa2a984e1cd98fc03ff7a965ca949b01

Add safe rules

src/tactic/aesop/tree.lean
@@ -91,7 +91,7 @@ meta structure node :=
 (cumulative_success_probability : percent)
 (rapps : list rapp_id)
 (failed_rapps : list regular_rule)
-(regular_queue : option (list regular_rule))
+(unsafe_queue : option (list unsafe_rule))
 (is_normal : bool)
 (is_proven : bool)
 (is_unprovable : bool)
@@ -101,8 +101,8 @@ namespace node
 
 protected meta def to_tactic_format (n : node) : tactic format := do
   goal ← format.of_goal n.goal,
-  regular_queue ←
-    match n.regular_queue with
+  unsafe_queue ←
+    match n.unsafe_queue with
     | some q := format.unlines <$> q.mmap pp
     | none := pure "<rules not yet selected>"
     end,
@@ -118,7 +118,7 @@ protected meta def to_tactic_format (n : node) : tactic format := do
       format! "is irrelevant: {n.is_irrelevant}\n",
       format! "successful rule applications: {n.rapps}\n",
       format! "rules waiting to be expanded:",
-      format.nested 2 regular_queue,
+      format.nested 2 unsafe_queue,
       format.line,
       format! "failed rule applications:",
       format.nested 2 $ format.unlines (n.failed_rapps.map to_fmt) ]
@@ -132,8 +132,8 @@ n.parent.is_none
 meta def is_unknown (n : node) : bool :=
 ¬ n.is_proven ∧ ¬ n.is_unprovable
 
-meta def has_no_unexpanded_rapp (n : node) : bool :=
-match n.regular_queue with
+meta def has_no_unexpanded_unsafe_rule (n : node) : bool :=
+match n.unsafe_queue with
 | none := ff
 | some q := q.empty
 end
@@ -210,7 +210,7 @@ meta def replace_node (id : node_id) (n : node) (t : tree) : tree :=
 meta def replace_rapp (rid : rapp_id) (r : rapp) (t : tree) : tree :=
 { rapps := t.rapps.insert rid r, ..t }
 
-meta def with_node' {α} [inhabited α] (id : node_id) (f : node → α) (t : tree) : α :=
+meta def with_node' {α} (id : node_id) (f : node → α) (t : tree) : α :=
 match t.get_node id with
 | some n := f n
 | none := undefined_core $
@@ -218,7 +218,7 @@ match t.get_node id with
 end
 
 meta def with_node (id : node_id) (f : node → tree) (t : tree) : tree :=
-@with_node' _ ⟨t⟩ id f t
+with_node' id f t
 
 meta def modify_node (id : node_id) (f : node → node) (t : tree) : tree :=
 t.with_node id $ λ n, t.replace_node id (f n)
@@ -235,7 +235,7 @@ end
 meta def get_rapps (ids : list rapp_id) (t : tree) : list (rapp_id × rapp) :=
 ids.filter_map $ λ id, (λ r, (id, r)) <$> t.get_rapp id
 
-meta def with_rapp' {α} [inhabited α] (id : rapp_id) (f : rapp → α) (t : tree) : α :=
+meta def with_rapp' {α} (id : rapp_id) (f : rapp → α) (t : tree) : α :=
 match t.get_rapp id with
 | some r := f r
 | none := undefined_core $
@@ -243,7 +243,7 @@ match t.get_rapp id with
 end
 
 meta def with_rapp (id : rapp_id) (f : rapp → tree) (t : tree) : tree :=
-@with_rapp' _ ⟨t⟩ id f t
+with_rapp' id f t
 
 meta def modify_rapp (rid : rapp_id) (f : rapp → rapp) (t : tree) : tree :=
 t.with_rapp rid $ λ r, t.replace_rapp rid (f r)
@@ -338,10 +338,14 @@ set_proven (sum.inr rid)
 meta def node_has_provable_rapp (n : node) (t : tree) : bool :=
 n.rapps.any $ λ id, t.with_rapp' id $ λ r, ¬ r.is_unprovable
 
+meta def node_may_have_unexpanded_rapp (n : node) (t : tree) : bool :=
+¬ n.has_no_unexpanded_unsafe_rule ∧
+n.rapps.all (λ id, t.with_rapp' id $ λ r, ¬ r.applied_rule.is_safe)
+
 meta def set_unprovable : sum node_id rapp_id → tree → tree :=
 modify_up
   (λ id n t,
-    if t.node_has_provable_rapp n ∨ ¬ n.has_no_unexpanded_rapp
+    if node_may_have_unexpanded_rapp n t
       then (t, ff)
       else
         -- Mark node as unprovable.

src/tactic/aesop/config.lean
@@ -50,45 +50,88 @@ meta def normalization_declaration_to_rule (decl : name) (c : normalization_rule
   end
 
 @[derive has_reflect]
-structure regular_rule_config :=
+meta structure safe_rule_config :=
+(penalty : option ℤ)
+(safety : option safety)
+
+namespace safe_rule_config
+
+meta def parser : lean.parser safe_rule_config := do
+  penalty ← optional small_int,
+  safety ← optional safety.parser,
+  pure { penalty := penalty, safety := safety }
+
+end safe_rule_config
+
+meta def safe_declaration_to_rule (decl : name) (c : safe_rule_config) :
+  tactic rule_set_member := do
+  env ← get_env,
+  d ← env.get decl,
+  let penalty := c.penalty.get_or_else 0,
+  let safety := c.safety.get_or_else safety.safe,
+  match d.type with
+  | `(tactic unit) := do
+    tac ← eval_expr (tactic unit) d.value,
+    pure $ rule_set_member.safe_rule
+      { tac := tac,
+        description := decl.to_string,
+        penalty := penalty,
+        safety := safety }
+      indexing_mode.unindexed
+  | _ := do
+    (r, imode) ← rule.apply_const decl,
+    let r : safe_rule :=
+      { penalty := penalty,
+        safety := safety,
+        ..r },
+    pure $ rule_set_member.safe_rule r imode
+  end
+
+@[derive has_reflect]
+structure unsafe_rule_config :=
 (success_probability : percent)
 
-namespace regular_rule_config
+namespace unsafe_rule_config
 
-meta def parser : lean.parser regular_rule_config := do
+meta def parser : lean.parser unsafe_rule_config := do
   success_probability ← percent.parser,
   pure ⟨success_probability⟩
 
-end regular_rule_config
+end unsafe_rule_config
 
-meta def regular_declaration_to_rule (decl : name) (c : regular_rule_config) :
+-- TODO major duplication
+meta def unsafe_declaration_to_rule (decl : name) (c : unsafe_rule_config) :
   tactic rule_set_member := do
   env ← get_env,
   d ← env.get decl,
   match d.type with
   | `(tactic unit) := do
     tac ← eval_expr (tactic unit) d.value,
-    pure $ rule_set_member.regular_rule
+    pure $ rule_set_member.unsafe_rule
       { tac := tac,
         description := decl.to_string,
         success_probability := c.success_probability }
       indexing_mode.unindexed
   | _ := do
-    (r, imode) ← rule.apply_const decl c.success_probability,
-    pure $ rule_set_member.regular_rule r imode
+    (r, imode) ← rule.apply_const decl,
+    let r : unsafe_rule :=
+      { success_probability := c.success_probability, ..r },
+    pure $ rule_set_member.unsafe_rule r imode
   end
 
 /-! ## Attribute -/
 
 @[derive has_reflect]
 meta inductive rule_config : Type
-| normalization (c : normalization_rule_config) : rule_config
-| regular (c : regular_rule_config) : rule_config
+| normalization (c : normalization_rule_config)
+| unsafe (c : unsafe_rule_config)
+| safe (c : safe_rule_config)
 
 meta def declaration_to_rule (decl : name) :
   rule_config → tactic rule_set_member
 | (rule_config.normalization c) := normalization_declaration_to_rule decl c
-| (rule_config.regular c) := regular_declaration_to_rule decl c
+| (rule_config.unsafe c) := unsafe_declaration_to_rule decl c
+| (rule_config.safe c) := safe_declaration_to_rule decl c
 
 meta def declarations_to_rule_set (decls : list (name × rule_config)) :
   tactic rule_set := do
@@ -104,7 +147,8 @@ meta def attr_config_parser : lean.parser rule_config := do
   rule_type ← optional ident,
   match rule_type with
   | some `norm := rule_config.normalization <$> normalization_rule_config.parser
-  | none := rule_config.regular <$> regular_rule_config.parser
+  | some `safe := rule_config.safe <$> safe_rule_config.parser
+  | none := rule_config.unsafe <$> unsafe_rule_config.parser
   | some n := fail $ format! "Unknown aesop attribute type: {n}"
   end
 
@@ -141,13 +185,17 @@ meta def rules_parser {α} (p : lean.parser α) : lean.parser (list (name × α)
 list_of (rule_parser p)
 
 meta def parser : lean.parser config_clause :=
-interactive.with_desc "(rules: [id probability, ...] | norm: [id penalty, ...])" $ do
+interactive.with_desc
+  "(unsafe_rules: [id probability, ...] | safe_rules: [id penalty, ...] | norm: [id penalty, ...])" $ do
   type ← ident,
   tk ":",
   match type with
-  | `rules :=
+  | `unsafe_rules :=
     additional_rules <$>
-      rules_parser (rule_config.regular <$> regular_rule_config.parser)
+      rules_parser (rule_config.unsafe <$> unsafe_rule_config.parser)
+  | `safe_rules :=
+    additional_rules <$>
+      rules_parser (rule_config.safe <$> safe_rule_config.parser)
   | `norm :=
     additional_rules <$>
       rules_parser (rule_config.normalization <$> normalization_rule_config.parser)

src/tactic/aesop/rule.lean
@@ -50,13 +50,13 @@ meta structure normalization_rule extends rule :=
 
 namespace normalization_rule
 
-meta def to_fmt (r : normalization_rule) : format :=
+protected meta def to_fmt (r : normalization_rule) : format :=
 format! "[{r.penalty}] {r.to_rule}"
 
 meta instance : has_to_format normalization_rule :=
 ⟨normalization_rule.to_fmt⟩
 
-meta def lt (r s : normalization_rule) : Prop :=
+protected meta def lt (r s : normalization_rule) : Prop :=
 r.penalty < s.penalty
 
 meta instance : has_lt normalization_rule :=
@@ -66,34 +66,117 @@ meta instance :
   decidable_rel ((<) : normalization_rule → normalization_rule → Prop) :=
 λ r s, (infer_instance : decidable (r.penalty < s.penalty))
 
-meta def ltb (r s : normalization_rule) : bool :=
+protected meta def ltb (r s : normalization_rule) : bool :=
 r < s
 
 end normalization_rule
 
-meta structure regular_rule extends rule :=
+@[derive has_reflect, derive decidable_eq]
+inductive safety
+| safe
+| almost_safe
+
+namespace safety
+
+protected meta def to_fmt : safety → format
+| safe := "safe"
+| almost_safe := "almost_safe"
+
+meta instance : has_to_format safety :=
+⟨safety.to_fmt⟩
+
+protected meta def parser : lean.parser safety :=
+interactive.with_desc "safe | almost_safe" $ do
+  i ← lean.parser.ident,
+  match i with
+  | `safe := pure safe
+  | `almost_safe := pure almost_safe
+  | _ := failed
+  end
+
+end safety
+
+meta structure safe_rule extends rule :=
+(penalty : ℤ)
+(safety : safety)
+
+namespace safe_rule
+
+protected meta def to_fmt (r : safe_rule) : format :=
+format! "[{r.penalty}] {r.to_rule}"
+
+meta instance : has_to_format safe_rule :=
+⟨safe_rule.to_fmt⟩
+
+protected meta def lt (r s : safe_rule) : Prop :=
+r.penalty < s.penalty
+
+meta instance : has_lt safe_rule :=
+⟨safe_rule.lt⟩
+
+meta instance :
+  decidable_rel ((<) : safe_rule → safe_rule → Prop) :=
+λ r s, (infer_instance : decidable (r.penalty < s.penalty))
+
+protected meta def ltb (r s : safe_rule) : bool :=
+r < s
+
+end safe_rule
+
+meta structure unsafe_rule extends rule :=
 (success_probability : percent)
+
+namespace unsafe_rule
+
+protected meta def to_fmt (r : unsafe_rule) : format :=
+format! "[{r.success_probability}] {r.to_rule}"
+
+meta instance : has_to_format unsafe_rule :=
+⟨unsafe_rule.to_fmt⟩
+
+protected meta def lt (r s : unsafe_rule) : Prop :=
+r.success_probability > s.success_probability
+
+meta instance : has_lt unsafe_rule :=
+⟨unsafe_rule.lt⟩
+
+meta instance :
+  decidable_rel ((<) : unsafe_rule → unsafe_rule → Prop) :=
+λ r s, (infer_instance : decidable (r.success_probability > s.success_probability))
+
+protected meta def ltb (r s : unsafe_rule) : bool :=
+r < s
+
+end unsafe_rule
+
+meta inductive regular_rule
+| safe (r : safe_rule)
+| unsafe (r : unsafe_rule)
 
 namespace regular_rule
 
-meta def to_fmt (r : regular_rule) : format :=
-format! "[{r.success_probability}] {r.to_rule}"
+protected meta def to_fmt : regular_rule → format
+| (safe r) := "[safe] " ++ r.to_fmt
+| (unsafe r) := "[unsafe] " ++ r.to_fmt
 
 meta instance : has_to_format regular_rule :=
 ⟨regular_rule.to_fmt⟩
 
-meta def lt (r s : regular_rule) : Prop :=
-r.success_probability > s.success_probability
+meta def to_rule : regular_rule → rule
+| (safe r) := r.to_rule
+| (unsafe r) := r.to_rule
 
-meta instance : has_lt regular_rule :=
-⟨regular_rule.lt⟩
+meta def success_probability : regular_rule → percent
+| (safe r) := ⟨100⟩
+| (unsafe r) := r.success_probability
 
-meta instance :
-  decidable_rel ((<) : regular_rule → regular_rule → Prop) :=
-λ r s, (infer_instance : decidable (r.success_probability > s.success_probability))
+meta def is_safe : regular_rule → bool
+| (safe _) := tt
+| (unsafe _) := ff
 
-meta def ltb (r s : regular_rule) : bool :=
-r < s
+meta def is_unsafe : regular_rule → bool
+| (safe _) := ff
+| (unsafe _) := tt
 
 end regular_rule
 
@@ -159,14 +242,16 @@ end rule_index
 meta inductive rule_set_member
 | normalization_rule (r : normalization_rule) (imode : indexing_mode)
 | normalization_simp_lemmas (s : simp_lemmas)
-| regular_rule (r : regular_rule) (imode : indexing_mode)
+| unsafe_rule (r : unsafe_rule) (imode : indexing_mode)
+| safe_rule (r : safe_rule) (imode : indexing_mode)
 
 /-! ## The Rule Set -/
 
 meta structure rule_set :=
 (normalization_rules : rule_index normalization_rule)
 (normalization_simp_lemmas : simp_lemmas)
-(regular_rules : rule_index regular_rule)
+(unsafe_rules : rule_index unsafe_rule)
+(safe_rules : rule_index safe_rule)
 
 namespace rule_set
 
@@ -175,7 +260,8 @@ protected meta def to_tactic_format (rs : rule_set) : tactic format := do
   pure $ format.unlines
     [ format! "normalization rules:{format.nested 2 $ rs.normalization_rules.to_fmt}",
       format! "normalization simp lemmas:{format.nested 2 simp_lemmas_fmt}",
-      format! "regular rules:{format.nested 2 $ rs.regular_rules.to_fmt}" ]
+      format! "safe rules:{format.nested 2 $ rs.safe_rules.to_fmt}",
+      format! "unsafe rules:{format.nested 2 $ rs.unsafe_rules.to_fmt}" ]
 
 meta instance : has_to_tactic_format rule_set :=
 ⟨rule_set.to_tactic_format⟩
@@ -183,7 +269,8 @@ meta instance : has_to_tactic_format rule_set :=
 meta def empty : rule_set :=
 { normalization_rules := rule_index.empty,
   normalization_simp_lemmas := simp_lemmas.mk,
-  regular_rules := rule_index.empty }
+  unsafe_rules := rule_index.empty,
+  safe_rules := rule_index.empty }
 
 meta def add_normalization_rule (r : normalization_rule) (imode : indexing_mode)
   (rs : rule_set) : rule_set :=
@@ -193,23 +280,30 @@ meta def add_normalization_simp_lemmas (s : simp_lemmas) (rs : rule_set) :
   rule_set :=
 { normalization_simp_lemmas := rs.normalization_simp_lemmas.join s, ..rs }
 
-meta def add_regular_rule (r : regular_rule) (imode : indexing_mode)
+meta def add_unsafe_rule (r : unsafe_rule) (imode : indexing_mode)
   (rs : rule_set) : rule_set :=
-{ regular_rules := rs.regular_rules.add r imode, ..rs }
+{ unsafe_rules := rs.unsafe_rules.add r imode, ..rs }
+
+meta def add_safe_rule (r : safe_rule) (imode : indexing_mode) (rs : rule_set) :
+  rule_set :=
+{ safe_rules := rs.safe_rules.add r imode, ..rs }
 
 meta def add_rule_set_member : rule_set_member → rule_set → rule_set
 | (rule_set_member.normalization_rule r imode) rs :=
   rs.add_normalization_rule r imode
 | (rule_set_member.normalization_simp_lemmas s) rs :=
   rs.add_normalization_simp_lemmas s
-| (rule_set_member.regular_rule r imode) rs :=
-  rs.add_regular_rule r imode
+| (rule_set_member.unsafe_rule r imode) rs :=
+  rs.add_unsafe_rule r imode
+| (rule_set_member.safe_rule r imode) rs :=
+  rs.add_safe_rule r imode
 
 meta def merge (rs₁ rs₂ : rule_set) : rule_set :=
-{ regular_rules := rs₁.regular_rules ++ rs₂.regular_rules,
+{ unsafe_rules := rs₁.unsafe_rules ++ rs₂.unsafe_rules,
   normalization_rules := rs₁.normalization_rules ++ rs₂.normalization_rules,
   normalization_simp_lemmas :=
-    rs₁.normalization_simp_lemmas.join rs₂.normalization_simp_lemmas }
+    rs₁.normalization_simp_lemmas.join rs₂.normalization_simp_lemmas,
+  safe_rules := rs₁.safe_rules ++ rs₂.safe_rules }
 
 meta instance : has_append rule_set :=
 ⟨merge⟩
@@ -221,8 +315,11 @@ meta def applicable_normalization_rules (rs : rule_set) :
   tactic (list normalization_rule) :=
 rs.normalization_rules.applicable_rules normalization_rule.ltb
 
-meta def applicable_regular_rules (rs : rule_set) : tactic (list regular_rule) :=
-rs.regular_rules.applicable_rules regular_rule.ltb
+meta def applicable_unsafe_rules (rs : rule_set) : tactic (list unsafe_rule) :=
+rs.unsafe_rules.applicable_rules unsafe_rule.ltb
+
+meta def applicable_safe_rules (rs : rule_set) : tactic (list safe_rule) :=
+rs.safe_rules.applicable_rules safe_rule.ltb
 
 end rule_set
 
@@ -232,6 +329,7 @@ namespace rule
 
 /-! ### Apply -/
 
+-- TODO open_pis is unnecessarily slow here
 meta def conclusion_head_constant (e : expr) :
   tactic (option name) := do
   (_, conclusion) ← open_pis e,
@@ -247,26 +345,24 @@ meta def apply_indexing_mode (type : expr) : tactic indexing_mode := do
     end
 
 /- Note: `e` may not be valid for the context in which this rule is going to be
-applied. Probably better to take a pexpr here. -/
-meta def apply (e : expr) (success_probability : percent) :
-  tactic (regular_rule × indexing_mode) := do
+applied. A maybe okay protocol:
+- Add `e` as an additional hyp with a generated pretty name.
+- Look `e` up by pretty name when the tactic is executed. --/
+meta def apply (e : expr) : tactic (rule × indexing_mode) := do
   type ← infer_type e,
   imode ← apply_indexing_mode type,
-  let r : regular_rule :=
+  let r : rule :=
     { tac := tactic.apply e >> skip,
-      success_probability := success_probability,
       description := format! "apply {e}" },
   pure (r, imode)
 
-meta def apply_const (n : name) (success_probability : percent) :
-  tactic (regular_rule × indexing_mode) := do
+meta def apply_const (n : name) : tactic (rule × indexing_mode) := do
   n ← resolve_constant n,
   env ← get_env,
   d ← env.get n,
   imode ← apply_indexing_mode d.type,
-  let r : regular_rule :=
+  let r : rule :=
     { tac := mk_const n >>= tactic.apply >> skip,
-      success_probability := success_probability,
       description := format! "apply {n}" },
   pure (r, imode)
 

src/tactic/aesop/search.lean
@@ -104,7 +104,7 @@ meta def add_node (s : state) (goal : expr) (success_probability : percent)
       cumulative_success_probability := success_probability,
       rapps := [],
       failed_rapps := [],
-      regular_queue := none,
+      unsafe_queue := none,
       is_normal := ff,
       is_proven := ff,
       is_unprovable := ff,
@@ -134,74 +134,79 @@ meta def initial_state : tactic state := do
   goal ← mk_meta_var tgt,
   prod.snd <$> add_node state.empty goal ⟨100⟩ none
 
-meta def run_rule (goal : expr) (r : regular_rule) : tactic (option (expr × list expr)) :=
+meta def run_rule (goal : expr) (r : tactic unit) :
+  tactic (option (expr × list expr)) :=
 with_local_goals' [goal] $ do
   tgt ← target,
   goal' ← mk_meta_var tgt,
   set_goals [goal'],
   try_core $ do
-    r.tac,
+    r,
     subgoals ← get_goals,
     pure (goal', subgoals)
 
+meta inductive rule_result
+| solved
+| failed
+| succeeded
+
 meta def apply_regular_rule (s : state) (parent_id : node_id)
-  (rule : regular_rule) : tactic state := do
+  (rule : regular_rule) : tactic (state × rule_result) := do
   let t := s.search_tree,
   parent ← t.get_node' parent_id "aesop/expand_rapp: internal error: ",
-  rule_result ← run_rule parent.goal rule,
+  result ← run_rule parent.goal rule.to_rule.tac,
   let success_probability :=
     parent.cumulative_success_probability * rule.success_probability,
-  s ← match rule_result with
-      | some (prf, []) := do
-          -- Rule succeeded and did not generate subgoals, meaning the parent
-          -- node is proven.
-          trace "rule solved the goal",
-          -- 1. Record the rule application.
-          let r : rapp :=
-            { applied_rule := rule,
-              cumulative_success_probability := success_probability,
-              proof := prf,
-              subgoals := [],
-              parent := parent_id,
-              is_proven := tt,
-              is_unprovable := ff,
-              is_irrelevant := ff },
-          trace $ pformat! "adding new rule application:{format.nested 2 <$> pp r}",
-          let (rid, t) := t.insert_rapp r,
-          -- 2. Mark parent node, and potentially ancestors, as proven.
-          let t := t.set_node_proven parent_id,
-          pure { search_tree := t, ..s }
-      | some (prf, subgoals) := do
-          -- Rule succeeded and generated subgoals.
-          trace "rule applied successfully",
-          -- 1. Record the rule application.
-          let r : rapp :=
-            { applied_rule := rule,
-              cumulative_success_probability := success_probability,
-              proof := prf,
-              subgoals := [],
-              parent := parent_id,
-              is_proven := ff,
-              is_unprovable := ff,
-              is_irrelevant := ff },
-          r_fmt ← pp r,
-          trace $ format! "recording new rapp:{format.nested 2 r_fmt}",
-          let (rid, t) := t.insert_rapp r,
-          let s := { search_tree := t, ..s },
-          -- 2. Record the subgoals.
-          (_, s) ← add_nodes s subgoals success_probability rid,
-          pure s
-      | none := do
-          -- Rule did not succeed.
-          trace "rule failed",
-          -- 1. Record rule failure.
-          let t := t.modify_node parent_id $ λ parent,
-            { failed_rapps := rule :: parent.failed_rapps, ..parent },
-          -- 2. Potentially mark parent node (and ancestors) as unprovable.
-          let t := t.set_node_unprovable parent_id,
-          pure { search_tree := t, ..s }
-      end,
-  pure s
+  match result with
+    | some (prf, []) := do
+        -- Rule succeeded and did not generate subgoals, meaning the parent
+        -- node is proven.
+        trace "rule solved the goal",
+        -- 1. Record the rule application.
+        let r : rapp :=
+          { applied_rule := rule,
+            cumulative_success_probability := success_probability,
+            proof := prf,
+            subgoals := [],
+            parent := parent_id,
+            is_proven := tt,
+            is_unprovable := ff,
+            is_irrelevant := ff },
+        trace $ pformat! "adding new rule application:{format.nested 2 <$> pp r}",
+        let (rid, t) := t.insert_rapp r,
+        -- 2. Mark parent node, and potentially ancestors, as proven.
+        let t := t.set_node_proven parent_id,
+        pure ({ search_tree := t, ..s }, rule_result.solved)
+    | some (prf, subgoals) := do
+        -- Rule succeeded and generated subgoals.
+        trace "rule applied successfully",
+        -- 1. Record the rule application.
+        let r : rapp :=
+          { applied_rule := rule,
+            cumulative_success_probability := success_probability,
+            proof := prf,
+            subgoals := [],
+            parent := parent_id,
+            is_proven := ff,
+            is_unprovable := ff,
+            is_irrelevant := ff },
+        r_fmt ← pp r,
+        trace $ format! "recording new rapp:{format.nested 2 r_fmt}",
+        let (rid, t) := t.insert_rapp r,
+        let s := { search_tree := t, ..s },
+        -- 2. Record the subgoals.
+        (_, s) ← add_nodes s subgoals success_probability rid,
+        pure (s, rule_result.succeeded)
+    | none := do
+        -- Rule did not succeed.
+        trace "rule failed",
+        -- 1. Record rule failure.
+        let t := t.modify_node parent_id $ λ parent,
+          { failed_rapps := rule :: parent.failed_rapps, ..parent },
+        -- 2. Potentially mark parent node (and ancestors) as unprovable.
+        let t := t.set_node_unprovable parent_id,
+        pure ({ search_tree := t, ..s }, rule_result.failed)
+    end
 
 meta def run_normalization_rule (r : normalization_rule) : tactic unit := do
   g ← get_goal,
@@ -257,19 +262,42 @@ meta def normalize_node_if_necessary (rs : rule_set) (s : state) (id : node_id)
 
 meta def select_rules_if_necessary (rs : rule_set) (s : state) (n : node) :
   tactic node := do
-  none ← pure n.regular_queue | pure n,
-  rules ← with_local_goals' [n.goal] $ rs.applicable_regular_rules,
+  none ← pure n.unsafe_queue | pure n,
+  rules ← with_local_goals' [n.goal] rs.applicable_unsafe_rules,
   trace $ format!
-    "applicable rules:{format.nested 2 $ format.unlines $ rules.map to_fmt}",
-  pure { regular_queue := rules, ..n }
+    "applicable unsafe rules:{format.nested 2 $ format.unlines $ rules.map to_fmt}",
+  pure { unsafe_queue := rules, ..n }
 
-meta def apply_first_regular_rule (s : state) (id : node_id) (n : node) :
+meta def apply_first_safe_rule_aux (id : node_id) :
+  state → list safe_rule → tactic (state × rule_result)
+| s [] := do
+  pure (s, rule_result.failed)
+| s (r :: rs) := do
+  trace $ format! "applying rule: {r}",
+  (s, result) ← apply_regular_rule s id (regular_rule.safe r),
+  match result with
+  | rule_result.failed := apply_first_safe_rule_aux s rs
+  | _ := pure (s, result)
+  end
+
+meta def apply_first_safe_rule (rs : rule_set) (s : state) (id : node_id) :
+  tactic (state × rule_result) := do
+  n ← s.search_tree.get_node' id "aesop/expand_node: internal error: ",
+  rules ← with_local_goals' [n.goal] rs.applicable_safe_rules,
+  trace $ format!
+    "applicable safe rules:{format.nested 2 $ format.unlines $ rules.map to_fmt}",
+  apply_first_safe_rule_aux id s rules
+
+-- TODO loop if rule failed instead of going back to main loop
+meta def apply_first_unsafe_rule (s : state) (id : node_id) :
   tactic state := do
-  some (rule :: rules) ← pure n.regular_queue | fail!
+  n ← s.search_tree.get_node' id "aesop/expand_node: internal error: ",
+  some (rule :: rules) ← pure n.unsafe_queue | fail!
     "aesop/expand_node: internal error: node {id} has no applicable rules",
   trace $ format! "applying rule: {rule}",
-  let s := s.modify_search_tree $ λ t, t.replace_node id { regular_queue := rules, ..n },
-  s ← apply_regular_rule s id rule,
+  let s := s.modify_search_tree $ λ t,
+    t.replace_node id { unsafe_queue := rules, ..n },
+  (s, _) ← apply_regular_rule s id (regular_rule.unsafe rule),
   pure $
     if rules.empty
       then s
@@ -281,9 +309,15 @@ meta def expand_node (rs : rule_set) (s : state) (id : node_id) :
   tactic state := do
   n ← s.search_tree.get_node' id "aesop/expand_node: internal error: ",
   n ← normalize_node_if_necessary rs s id n,
-  n ← select_rules_if_necessary rs s n,
   let s := s.modify_search_tree $ λ t, t.replace_node id n,
-  apply_first_regular_rule s id n
+  (s, result) ← apply_first_safe_rule rs s id,
+  match result with
+  | rule_result.failed := do
+    n ← select_rules_if_necessary rs s n,
+    let s := s.modify_search_tree $ λ t, t.replace_node id n,
+    apply_first_unsafe_rule s id
+  | _ := pure s
+  end
 
 meta def expand (rs : rule_set) (s : state) : tactic state := do
   trace "=====================================================================",

test/aesop/even_or_odd.lean
@@ -18,8 +18,9 @@ inductive EvenOrOdd : ℕ → Prop
 | even {n} : Even n → EvenOrOdd n
 | odd {n} : Odd n → EvenOrOdd n
 
-attribute [aesop  50] EvenOrOdd.even
-attribute [aesop 100] EvenOrOdd.odd Even.zero Even.plus_two Odd.one
+attribute [aesop safe 0] EvenOrOdd.even
+attribute [aesop  50] EvenOrOdd.odd
+attribute [aesop 100] Even.zero Even.plus_two Odd.one
 
 def even_or_odd (n : ℕ) : Prop := EvenOrOdd n
 
@@ -29,5 +30,10 @@ meta def test_norm_tactic : tactic unit := `[try { rw [even_or_odd_def] at * }]
 
 meta def test_regular_tactic : tactic unit := `[apply Odd.plus_two]
 
+set_option trace.aesop.steps true
+
 example : even_or_odd 3 :=
-by aesop rules: [test_regular_tactic 50], norm: [test_norm_tactic]
+by aesop unsafe_rules: [test_regular_tactic 50], norm: [test_norm_tactic]
+
+example : even_or_odd 2 :=
+by aesop unsafe_rules: [test_regular_tactic 50], norm: [test_norm_tactic]

commit 327cd9ac286f2effeb8ae9568c59849a57f26015

Fix a bug where nodes were spuriously marked as unprovable.
This bug was introduced in 1f8e7c8bfa2a984e1cd98fc03ff7a965ca949b01.

src/tactic/aesop/tree.lean
@@ -340,12 +340,12 @@ n.rapps.any $ λ id, t.with_rapp' id $ λ r, ¬ r.is_unprovable
 
 meta def node_may_have_unexpanded_rapp (n : node) (t : tree) : bool :=
 ¬ n.has_no_unexpanded_unsafe_rule ∧
-n.rapps.all (λ id, t.with_rapp' id $ λ r, ¬ r.applied_rule.is_safe)
+¬ n.rapps.any (λ id, t.with_rapp' id $ λ r, r.applied_rule.is_safe)
 
 meta def set_unprovable : sum node_id rapp_id → tree → tree :=
 modify_up
   (λ id n t,
-    if node_may_have_unexpanded_rapp n t
+    if node_may_have_unexpanded_rapp n t ∨ node_has_provable_rapp n t
       then (t, ff)
       else
         -- Mark node as unprovable.